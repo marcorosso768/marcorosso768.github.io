3aa33c543db4e41684b1317370b3a0e11182ac0f
Marco Rosso <m.rosso@me.com>
2024-10-23 19:42:42 -0300

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -21,7 +21,7 @@ let setThemeSetting = (themeSetting) => {
 
 // Apply the computed dark or light theme to the website.
 let applyTheme = () => {
-  let theme = determineThemeSetting();
+  let theme = determineComputedTheme();
 
   transTheme();
   setHighlight(theme);
@@ -203,15 +203,30 @@ let transTheme = () => {
   }, 500);
 };
 
-// Determine the expected state of the theme toggle, which can be "dark" or "light". Default is the user's system theme.
+// Determine the expected state of the theme toggle, which can be "dark" or "light".
+// Default is the user's system theme.
 let determineThemeSetting = () => {
   let themeSetting = localStorage.getItem("theme");
   if (themeSetting != "dark" && themeSetting != "light") {
     const userPref = window.matchMedia;
     if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
-      return themeSetting="dark";
+      return "dark";
     } else {
-      return themeSetting="light";
+      return "light";
+  }
+  return themeSetting
+};
+
+// Determine the computed theme, which can be "dark" or "light". If not, 
+// the computed theme is determined based on the user's system preference.
+let determineComputedTheme = () => {
+  let themeSetting = determineThemeSetting();
+    const userPref = window.matchMedia;
+    if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
+      return "dark";
+    } else {
+      return "light";
+    }
   }
 };
 
