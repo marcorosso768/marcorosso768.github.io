4e4ce9cfd273624f1f463561b54a403b9b65c175
Marco Rosso <m.rosso@me.com>
2024-11-02 13:52:22 -0300

    Update language-toggle.js

--- assets/js/language-toggle.js
+++ assets/js/language-toggle.js
@@ -10,18 +10,23 @@ function languageURL(language) {
   const defaultLanguage = 'en'; // Default language is English
 
   if (language === defaultLanguage) {
-    return `{{ site.baseurl }}${currentURL}`; // Default: no prefix
+    return `{{ site.baseurl }}${currentURL}`; // No prefix for default language
   } else {
     return `{{ site.baseurl }}/${language}${currentURL}`; // Add prefix for other languages
   }
 }
 
-// Redirect to preferred language on page load
+// Redirect to preferred language on page load, defaulting to English
 document.addEventListener("DOMContentLoaded", function() {
+  // Always start with English unless a choice has already been saved
   const userLanguage = localStorage.getItem('userLanguage') || 'en';
   const currentURL = window.location.pathname;
 
+  // Redirect to stored language if different from the default (English) or current path
   if (userLanguage !== 'en' && !currentURL.startsWith(`/${userLanguage}`)) {
     window.location.href = `{{ site.baseurl }}/${userLanguage}${currentURL}`;
+  } else if (userLanguage === 'en' && currentURL.startsWith(`/${userLanguage}`)) {
+    // Redirect to base URL if in English and there is an unnecessary prefix
+    window.location.href = `{{ site.baseurl }}${currentURL.substring(userLanguage.length + 1)}`;
   }
 });
