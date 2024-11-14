b17b383d54c2ce09b77c662afd6c2e2c6d784daf
Marco Rosso <m.rosso@me.com>
2024-11-14 15:04:16 -0300

    Update language-toggle.js

--- assets/js/language-toggle.js
+++ assets/js/language-toggle.js
@@ -1,23 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const urlMap = {{ site.data.url_map | jsonify }};
+  const urlMap = JSON.parse(document.getElementById('urlMapData').textContent);
   const defaultLanguage = 'en';
   const currentPageId = document.body.getAttribute('data-page-id');
   const userLanguage = localStorage.getItem('userLanguage') || defaultLanguage;
 
-  // Language Toggle Function
   function setLanguage(language) {
     if (!urlMap[language] || !currentPageId) return;
-
     const newPath = urlMap[language][currentPageId] || urlMap[defaultLanguage][currentPageId];
     localStorage.setItem('userLanguage', language);
-
-    // Redirect only if the new path is different from the current one
     if (newPath && newPath !== window.location.pathname) {
       window.location.href = newPath;
     }
   }
 
-  // Check User Language Preference and Redirect on Homepage
   const isHomepage = (currentPageId === 'about');
   if (isHomepage && userLanguage !== defaultLanguage) {
     const newURL = urlMap[userLanguage]['about'];
@@ -26,6 +21,5 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  // Expose setLanguage for the toggle dropdown
   window.setLanguage = setLanguage;
 });
