5afdc6f1b9c46beb924f227740ee35ccd33772e0
Marco Rosso <m.rosso@me.com>
2025-05-13 22:06:42 +0200

    Update toggle-sections.js

--- assets/js/toggle-sections.js
+++ assets/js/toggle-sections.js
@@ -5,14 +5,10 @@ function toggleVisibility(id) {
   const isVisible = section.style.display === "block";
   section.style.display = isVisible ? "none" : "block";
 
-  // Ruota la freccia
   if (chevron) {
     chevron.classList.toggle("rotated", !isVisible);
-    chevron.classList.remove("fa-chevron-down", "fa-chevron-right");
-    chevron.classList.add(!isVisible ? "fa-chevron-down" : "fa-chevron-right");
   }
 
-  // Chiude eventuali abstract aperti nella sezione
   if (isVisible) {
     section.querySelectorAll(".abstract").forEach(el => el.classList.remove("open"));
   }
