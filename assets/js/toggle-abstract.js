8e5814863b34cd64e89845989c7872d908970cf3
Marco Rosso <m.rosso@me.com>
2025-05-13 18:32:31 +0200

    Update toggle-abstract.js

--- assets/js/toggle-abstract.js
+++ assets/js/toggle-abstract.js
@@ -1,12 +1,12 @@
 function toggleAbstract(id, showText, hideText) {
   const abstract = document.getElementById(id);
+  const toggle = document.getElementById("toggle-" + id);
   const icon = document.getElementById("icon-" + id);
-  const label = document.getElementById("label-" + id);
   const isOpen = abstract.classList.contains("open");
 
-  // Chiudi tutto
+  // Chiudi tutti
   document.querySelectorAll(".abstract").forEach(el => el.classList.remove("open"));
-  document.querySelectorAll(".toggle-label").forEach(el => el.innerText = showText);
+  document.querySelectorAll(".abstract-toggle-pill").forEach(el => el.title = showText);
   document.querySelectorAll(".abstract-toggle-pill i").forEach(el => {
     el.className = "fa-solid fa-chevron-right fa-2xs";
   });
@@ -15,6 +15,6 @@ function toggleAbstract(id, showText, hideText) {
   if (!isOpen) {
     abstract.classList.add("open");
     icon.className = "fa-solid fa-chevron-down fa-2xs";
-    label.innerText = hideText;
+    toggle.title = hideText;
   }
 }
