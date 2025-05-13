241d48041854db3fee8df575861debcd474b212b
Marco Rosso <m.rosso@me.com>
2025-05-13 19:12:09 +0200

    Update toggle-abstract.js

--- assets/js/toggle-abstract.js
+++ assets/js/toggle-abstract.js
@@ -2,15 +2,18 @@ function toggleAbstract(id, showText, hideText) {
   const abstract = document.getElementById(id);
   const toggle = document.getElementById("toggle-" + id);
   const icon = document.getElementById("icon-" + id);
+  const label = document.getElementById("label-" + id);
   const isOpen = abstract.classList.contains("open");
 
   if (isOpen) {
     abstract.classList.remove("open");
-    toggle.title = showText;
     icon.style.transform = "rotate(0deg)";
+    toggle.title = showText;
+    label.classList.remove("open");
   } else {
     abstract.classList.add("open");
-    toggle.title = hideText;
     icon.style.transform = "rotate(90deg)";
+    toggle.title = hideText;
+    label.classList.add("open");
   }
 }
