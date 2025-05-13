c7e56da78cf2c4fb74a615c1e835655ad63e602f
Marco Rosso <m.rosso@me.com>
2025-05-13 19:12:29 +0200

    Update toggle-sections.js

--- assets/js/toggle-sections.js
+++ assets/js/toggle-sections.js
@@ -1,26 +1,15 @@
 function toggleVisibility(id) {
   const content = document.getElementById(id);
-  const toggleButton = document.getElementById('toggle-' + id).querySelector('i');
+  const toggle = document.getElementById(`toggle-${id}`);
+  const icon = toggle.querySelector("i");
 
   const isVisible = content.style.display !== "none";
 
   if (isVisible) {
     content.style.display = "none";
-    toggleButton.className = "fa-solid fa-chevron-right fa-2xs";
-
-    // Chiude tutti gli abstract dentro la sezione chiusa
-    content.querySelectorAll(".abstract").forEach(el => {
-      el.classList.remove("open");
-    });
-    content.querySelectorAll(".abstract-toggle-pill").forEach(el => {
-      el.title = el.dataset.show;
-    });
-    content.querySelectorAll(".abstract-toggle-pill i").forEach(el => {
-      el.style.transform = "rotate(0deg)";
-    });
-
+    icon.style.transform = "rotate(0deg)";
   } else {
     content.style.display = "block";
-    toggleButton.className = "fa-solid fa-chevron-down fa-2xs";
+    icon.style.transform = "rotate(90deg)";
   }
 }
